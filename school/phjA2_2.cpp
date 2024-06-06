f0d60d722ad8e8befae8add3c4b8cca733380cd3
PJunior17 <101608084+PJunior17@users.noreply.github.com>
2024-06-06 14:34:34 -0400

    wrote base code
    need to add description and rest of report

---
+++ school/phjA2_2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+	float balance;
+	std::cout << "Enter the starting balance on the account: ";
+	std::cin >> balance;	
+	
+	float interest_rate;
+	std::cout << "Enter annual interest rate: ";
+	std::cin >> interest_rate;
+	std::cout << "\n";
+	
+	int month = 1;
+	float start_month_balance = balance;
+	float end_month_balance = start_month_balance;
+	float monthly_interest_rate = interest_rate / 12;
+	float monthly_interest_added;
+
+	float total_deposited;
+	float total_withdrawn;
+	float total_interest;
+	do {
+		std::cout << std::fixed;
+		std::cout << "Month " << month << std::endl;
+		float monthly_deposit;
+		std::cout << "Total deposited for the month: $";
+		std::cin >> monthly_deposit;
+		end_month_balance += monthly_deposit;
+		total_deposited += monthly_deposit;
+
+		float monthly_withdrawn;
+		std::cout << "Total withdrawn for the month: $";
+		std::cin >> monthly_withdrawn;
+		end_month_balance -= monthly_withdrawn;
+		total_withdrawn += monthly_withdrawn;
+
+		monthly_interest_added = monthly_interest_rate * ((start_month_balance + end_month_balance) / 2);
+		std::cout << "Interest recieved this month: " << std::setprecision(2) << "$" <<  monthly_interest_added << std::endl;
+		total_interest += monthly_interest_added;
+
+		end_month_balance += monthly_interest_added;
+		std::cout << "Ending monthly balance: " << std::setprecision(2) << "$" << end_month_balance << "\n\n";
+		
+		start_month_balance = end_month_balance;
+		month++;	
+	} while(month < 4);
+	
+	std::cout << "Quarterly Savings Account Statement\n\n";
+	std::cout << "Starting Balance: " << balance << std::endl;
+	std::cout << "Total Deposits: " << total_deposited << std::endl;
+	std::cout << "Total Withdrawls: " << total_withdrawn << std::endl;
+	std::cout << "Total Interest: " << total_interest << std::endl;
+	std::cout << "________________" << std::endl;
+	std::cout << "Ending Balance: " << end_month_balance << std::endl;
+
+	std::cout << "----------------------------------------------------------\n";
+
+	return 0;
+}
+
